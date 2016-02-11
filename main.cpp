b485c5d38b77595cf974110eb5fad9a5ae9154a6
Wyatt Sullivan <wsull001@ucr.edu>
2016-02-10 23:03:47 -0800

    adding main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <unistd.h>
+#include "Terminal.h"
+#include "Command.h"
+#include "CmdFramer.h"
+#include "Executor.h"
+#include "ExecutorBase.h"
+using namespace std;
+
+void testFunct(char* &p) {
+    string f = "hello";
+    p = (char*)f.c_str();
+}
+
+int main() {
+    
+    
+    Terminal t;
+    t.run();
+    
+    return 0;
+}
