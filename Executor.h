40ef5d94f6d8c87a1eb0997cc6ff5e2445fbdaf9
Srisri Gokanapudy <sgoka001@hammer.cs.ucr.edu>
2016-02-10 23:24:19 -0800

    Added executor.h

---
+++ Executor.h
@@ -0,0 +1,40 @@
+#ifndef EXECUTOR_H
+#define EXECUTOR_H
+#include <unistd.h>
+#include <iostream>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <cstdlib>
+#include <string.h>
+#include "Command.h"
+#include "ExecutorBase.h"
+using namespace std;
+
+class Executor : public ExecutorBase {
+    public:
+    Executor() {};
+    virtual ~Executor() {};
+    int run_cmd (Command* cmd) {
+        char** args = cmd->getArgs();
+        
+        if (strcmp(args[0], "exit") == 0) {
+            exit(0);
+        }
+        
+        
+        int procID, status;
+        
+        procID = fork();
+        
+        if (procID == 0) {
+            int t = execvp(args[0], args);
+            exit(t);
+        }
+        wait(&status);
+        
+        return status;
+    
+    };
+};
+
+#endif
